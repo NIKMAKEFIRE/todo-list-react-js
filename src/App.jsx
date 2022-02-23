571d4b7cd782685b14d5cd2e0e25fb892ee62010
NIKMAKEFIRE <81253144+NIKMAKEFIRE@users.noreply.github.com>
2022-02-23 20:58:04 +0300

    react-todo

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import List from './components/List';
+import listSvg from './assets/img/list.svg'
+
+function App() {
+  return (
+    <div className="todo">
+      <div className="todo__sidebar">
+        <List items={[
+          {
+            icon: <img src={listSvg} alt="List icon" />,
+            name: 'Все задачи',
+            active: true
+          },
+        ]}/>
+        <List items={[
+          {
+            color: 'green',
+            name: 'Покупки'
+          },
+          {
+            color: 'blue',
+            name: 'Фронтенд'
+          },
+          {
+            color: 'pink',
+            name: 'Фильмы и сериалы'
+          },
+        ]}/>
+      </div>
+      <div className="todo__tasks">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
